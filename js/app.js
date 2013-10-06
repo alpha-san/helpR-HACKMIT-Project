422c42979c51ede4084a2c3593d09a9dcc558f49
Allan Farinas <awesomeplusplus@riseup.net>
2013-10-06 02:57:32 -0700

    Opps, added the js/app.js this time for real

--- js/app.js
+++ js/app.js
@@ -34,3 +34,28 @@ Handlebars.registerHelper("getAllEventsTable", function() {
    s += "</table>";
    return s;
 });
+
+Handlebars.registerHelper("submitNewEvent", function() {
+    var eventRef = new Firebase("https://cal-poly-hackmit.firebaseio.com/events");
+
+    var newPushRef = myDataRef.push();
+            var name = $('#eventNameInput').val();
+            var street = $('#streetInput').val(); 
+            var city = $('#cityInput').val();
+            var state = $('#stateInput').val(); 
+            var zipCode = $('#zipCodeInput').val(); 
+            var dateThing = $('#dateInput').val(); 
+            var startTime = $('#startTimeInput').val(); 
+            var endTime = $('#endTimeInput').val(); 
+            var obj = {
+              "name":name,
+             "street":street,
+             "city":city,
+             "zip-code":zipCode,
+             "state":state,
+             "date":dateThing,
+             "start-time":startTime,
+             "end-time":endTime
+            };
+            newPushRef.set(obj);    
+});
