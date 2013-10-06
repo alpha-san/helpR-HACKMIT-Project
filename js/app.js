68d41f9beb312f2ab45a4c7e0b0f7c9a8a3875e6
Daniel Goodrich <dgoodrich.radioflag@gmail.com>
2013-10-06 04:06:27 -0400

    Added event call function

--- js/app.js
+++ js/app.js
@@ -5,3 +5,32 @@ App.Router.map(function() {
   this.resource('contact');
   this.resource('events');
 });
+
+Handlebars.registerHelper("getAllEventsTable", function() {
+   var eventRef = new Firebase("https://cal-poly-hackmit.firebaseio.com/events");
+   var s =  "<table class='table'>";
+   var headers = false;
+   eventRef.on('value', function(snapshot){
+      if ( snapshot == null ){
+         s += "<tr><td>null</td></tr>";
+      }
+      snapshot.forEach(function(childSnapshot){
+         var childVal = childSnapshot.val();
+         var key;
+         s += "<tr>";
+         if (headers == false){
+            for ( key in childVal ){
+               s += "<td><b>" + key + "</b></td>";
+            }
+            s += "</tr><tr>"
+            headers = true;
+         }
+         for ( key in childVal ){
+            s += "<td>" + childVal[key] + "</td>";
+         }
+         s += "</tr>";
+      });
+   });
+   s += "</table>";
+   return s;
+});
