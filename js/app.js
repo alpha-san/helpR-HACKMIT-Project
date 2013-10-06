72ac26c0944fe7adad5df62d3ceeae0d22e62b8b
Nymerias <nicolette.yadegar@gmail.com>
2013-10-06 07:30:27 -0400

    Working events

--- js/app.js
+++ js/app.js
@@ -8,49 +8,48 @@ App.Router.map(function() {
 
 Handlebars.registerHelper("getAllEventStubs", function() {
    var eventRef = new Firebase("https://cal-poly-hackmit.firebaseio.com/events");
-   var s =  "<section id='events'>";
-   var headers = false;
-   eventRef.on('value', function(snapshot){
+   var s =  "<section class='event-list'>";
+     eventRef.on('value', function(snapshot){
       if ( snapshot == null ){
          s += "<h2>Null Title</h2><p>null description</p>";
       }
       snapshot.forEach(function(childSnapshot){
          var childVal = childSnapshot.val();
          var key;
-   		var areaCode;
-   		var city;
-   		var endTime;
-   		var name;
-   		var startTime;
-   		var state;
-   		var street;
-   		var date;
+		 var areaCode;
+		 var city;
+		 var endTime;
+		 var name;
+		 var startTime;
+		 var state;
+		 var street;
+		 var date;
          
-         s += "<div class = 'event'> <img width='200' height='135'/>";
+         s += "<div class='event-list'><img width='200' height='135'/>";
 		 
-         for ( key in childVal ){
-   			if (key == "area-code")
-   				return areaCode = childVal[key];
-   			else if (key == "city")
-   				return city = childVal[key];
-   			else if (key == "end-time")
-   				return endTime = childVal[key];
-   			else if (key == "name")
-   				return name = childVal[key];
-   			else if (key == "start-time")
-   				return startTime = childVal[key];
-   			else if (key == "state")
-   				return state = childVal[key];
-   			else if (key == "street")
-   				return street = childVal[key];
-   			else if (key == "date")
-   				return date = childVal[key];
+         for (key in childVal){
+   			if (key === "area-code")
+   				areaCode = childVal[key];
+   			else if (key === "city")
+   				city = childVal[key];
+   			else if (key === "end-time")
+   				endTime = childVal[key];
+   			else if (key === "name")
+   				name = childVal[key];	
+   			else if (key === "start-time")
+   				startTime = childVal[key];
+   			else if (key === "state")
+   				state = childVal[key];
+   			else if (key === "street")
+   				street = childVal[key];
+   			else if (key === "date")
+   				date = childVal[key];
          }
 		 
-		   s += "<h2>" + name + "</h2>";
-         s += "<p>" + startTime + "</p>";
-         s += "<p>" + endTime + "</p>";
-         s += "<footer>" + date + "</footer>";
+		 s += "<h2>" + name + "</h2>";
+         s += "<p>Start time: " + startTime + "</p>";
+         s += "<p>End time: " + endTime + "</p>";
+         s += "<p>Date: " + date + "</p>";
          s += "</div>";
       });
    });
